c3fc3de6750d76b3d8482f8402cbc432ade15969
Alexander Balashov <balashov187@yandex.ru>
2023-03-15 21:32:23 +0300

    задача 21 семина 3

--- Program.cs
+++ Program.cs
@@ -7,20 +7,44 @@
 // 12821 -> да
 
 // 23432 -> да
-System.Console.WriteLine("Введите 5-ти значное число ");
-string number = Console.ReadLine()!;
-if (number.Length == 5)
+// System.Console.WriteLine("Введите 5-ти значное число ");
+// string number = Console.ReadLine()!;
+// if (number.Length == 5)
+// {
+//     if (number[0]==number[4]&&number[1]==number[3])
+//     {
+//         Console.WriteLine($"{number} -> да ");
+//     }
+//     else
+//     {
+//         Console.WriteLine($"{number} -> нет ");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Введено неверно число");
+// }
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int ReadInt(string text)
 {
-    if (number[0]==number[4]&&number[1]==number[3])
-    {
-        Console.WriteLine($"{number} -> да ");
-    }
-    else
-    {
-        Console.WriteLine($"{number} -> нет ");
-    }
+    System.Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
 }
-else
-{
-    Console.WriteLine("Введено неверно число");
-}
+
+int[] a = new int[3];
+int[] b = new int[3];
+
+a[0] = ReadInt("Введите координату х точки а ");
+a[1] = ReadInt("Введите координату y точки а ");
+a[2] = ReadInt("Введите координату z точки а ");
+b[0] = ReadInt("Введите координату х точки b ");
+b[1] = ReadInt("Введите координату y точки b ");
+b[2] = ReadInt("Введите координату z точки b ");
+
+double res = Math.Round(Math.Sqrt(Math.Pow(a[0]-b[0], 2)+ Math.Pow(a[1]-b[1], 2)+Math.Pow(a[2]-b[2], 2)),2);
+System.Console.WriteLine("Растояние между точками  =" + res);
