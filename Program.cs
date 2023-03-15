a86ed5b212655c73380b0d3439be289d464950d2
Alexander Balashov <balashov187@yandex.ru>
2023-03-15 21:25:30 +0300

    задача 19 семина 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+System.Console.WriteLine("Введите 5-ти значное число ");
+string number = Console.ReadLine()!;
+if (number.Length == 5)
+{
+    if (number[0]==number[4]&&number[1]==number[3])
+    {
+        Console.WriteLine($"{number} -> да ");
+    }
+    else
+    {
+        Console.WriteLine($"{number} -> нет ");
+    }
+}
+else
+{
+    Console.WriteLine("Введено неверно число");
+}
